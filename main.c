d3dba7115d98e3d32e53e582d3bdb57c5e7fe8ed
Romain ROUSSEL <rorousse@e3r9p4.42.fr>
2016-03-02 14:08:35 +0100

    temp

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rorousse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/26 18:04:48 by rorousse          #+#    #+#             */
-/*   Updated: 2016/03/02 11:54:43 by rorousse         ###   ########.fr       */
+/*   Updated: 2016/03/02 14:08:25 by rorousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ int main()
 	int	taillemine;
 
 	setlocale(LC_ALL, "en_US.UTF-8");
-	tailleor = printf("%#O\n", 1);
-	taillemine = ft_printf("%#O\n",1);
+	tailleor = printf("%+O\n", 0);
+	taillemine = ft_printf("%+O\n", 0);
 	printf("original %d mien %d\n",tailleor,taillemine);
 	return (0);
 }
